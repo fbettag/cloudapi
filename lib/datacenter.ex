649fa34201a700460e771a16417da174053ac1bc
Franz Bettag <franz@bett.ag>
2019-10-30 12:36:38 +0100

    makes CloudAPI.Datacenter a proper Ecto schema so i can emebed it in other Ecto models.

--- lib/datacenter.ex
+++ lib/datacenter.ex
@@ -2,5 +2,14 @@ defmodule CloudAPI.Datacenter do
   @typedoc """
   This structure represents a CloudAPI Datacenter used to communicate with a Triton Cluster.
   """
-  defstruct endpoint: "", account: "", keyfile: "", keyname: ""
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  embedded_schema do
+    field :endpoint, :string
+    field :account, :string
+    field :keyfile, :string
+    field :keyname, :string
+  end
 end
