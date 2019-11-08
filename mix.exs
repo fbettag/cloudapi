3750b8a26fefdc382daee5ea1e692c5ec0c0858b
Franz Bettag <franz@bett.ag>
2019-11-08 22:30:11 +0100

    removes the terrible git_version helper.

--- mix.exs
+++ mix.exs
@@ -4,15 +4,14 @@ defmodule CloudAPI.Mixfile do
   @project_url "https://github.com/fbettag/cloudapi"
 
   def project do
-    {tag, description} = git_version()
     [
       app:              :cloudapi,
-      version:          tag,
+      version:          "0.0.6",
       elixir:           "~> 1.4",
       source_url:       @project_url,
       homepage_url:     @project_url,
       name:             "Joyent CloudAPI",
-      description:      "This package implements the full CloudAPI for managing Joyent Triton clusters " <> description,
+      description:      "This package implements the full CloudAPI for managing Joyent Triton clusters",
       build_embedded:   Mix.env == :prod,
       start_permanent:  Mix.env == :prod,
       package:          package(),
@@ -49,28 +48,5 @@ defmodule CloudAPI.Mixfile do
       test: ["test", "credo -a --strict"],
       "hex.publish": ["test", "hex.publish"],
     ]
-  end
-
-  defp git_version() do
-    # pulls version information from "nearest" git tag or sha hash-ish
-    {hashish, 0} =
-      System.cmd("git", ~w[describe --dirty --abbrev=7 --tags --always --first-parent])
-
-    full_version = String.trim(hashish)
-
-    tag_version =
-      hashish
-      |> String.split("-")
-      |> List.first()
-      |> String.replace_prefix("v", "")
-      |> String.trim()
-
-    tag_version =
-      case Version.parse(tag_version) do
-        :error -> "0.0.0-#{tag_version}"
-        _ -> tag_version
-      end
-
-    {tag_version, full_version}
   end
 end
